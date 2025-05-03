83548ad4c57308d43ee65a0a40119643a65d1c02
Egor Andronaki <egorandronaki@gmail.com>
2025-05-03 18:18:12 +0300

    fixed imports

--- app/src/main/java/com/example/ft/search/search_flights/FlightSearchScreen.kt
+++ app/src/main/java/com/example/ft/search/search_flights/FlightSearchScreen.kt
@@ -2,7 +2,6 @@ package com.example.ft.search.search_flights
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
